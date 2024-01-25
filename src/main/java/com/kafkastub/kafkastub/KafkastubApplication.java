744fb3b63ba001762f72940ec0ddc636b7ab22ce
Kawaiiskii <antonzhadnov@gmail.com>
2024-01-25 15:12:17 +0300

    init commit

---
+++ src/main/java/com/kafkastub/kafkastub/KafkastubApplication.java
@@ -0,0 +1,13 @@
+package com.kafkastub.kafkastub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkastubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkastubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafkastub/kafkastub/KafkastubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkastub.kafkastub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkastubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
