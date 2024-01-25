55712ca657c0f90bf33c8c0435b89283060369b3
Kawaiiskii <antonzhadnov@gmail.com>
2024-01-25 18:42:25 +0300

    add and implement Message

---
+++ src/main/java/com/kafkastub/kafkastub/message/Message.java
@@ -0,0 +1,40 @@
+package com.kafkastub.kafkastub.message;
+
+import java.util.UUID;
+
+public class Message {
+    private String uuid;
+    private String data;
+
+    public Message() {
+    }
+
+    public Message(String data) {
+        this.uuid = UUID.randomUUID().toString();
+        this.data = data;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "uuid='" + uuid + '\'' +
+                ", data='" + data + '\'' +
+                '}';
+    }
+}
