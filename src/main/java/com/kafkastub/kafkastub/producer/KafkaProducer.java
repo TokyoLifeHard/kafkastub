26bd33a21d231d6ec75cbef390ec5275f4379b52
Kawaiiskii <antonzhadnov@gmail.com>
2024-01-25 15:49:47 +0300

    implemented KafkaProducer

--- src/main/java/com/kafkastub/kafkastub/producer/KafkaProducer.java
+++ src/main/java/com/kafkastub/kafkastub/producer/KafkaProducer.java
@@ -1,4 +1,23 @@
 package com.kafkastub.kafkastub.producer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class KafkaProducer {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+
+    private KafkaTemplate<String,String> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void send(String message){
+        LOGGER.info(String.format("Send message %s",message));
+        this.kafkaTemplate.send("in",message);
+    }
 }
