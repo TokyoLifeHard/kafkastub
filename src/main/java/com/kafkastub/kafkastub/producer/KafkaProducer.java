01e8463f936929e7f2a2b6887da11013593c1890
Kawaiiskii <antonzhadnov@gmail.com>
2024-03-09 12:47:34 +0300

    refactor KafkaProducer

--- src/main/java/com/kafkastub/kafkastub/producer/KafkaProducer.java
+++ src/main/java/com/kafkastub/kafkastub/producer/KafkaProducer.java
@@ -2,8 +2,13 @@ package com.kafkastub.kafkastub.producer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Executor;
 
 @Component
 public class KafkaProducer {
@@ -12,12 +17,16 @@ public class KafkaProducer {
 
     private KafkaTemplate<String,String> kafkaTemplate;
 
+    @Autowired
+    @Qualifier("writePoolTaskExecutor")
+    private Executor executor;
+
     public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
     public void send(String message){
         LOGGER.info(String.format("Send message %s",message));
-        this.kafkaTemplate.send("in",message);
+        executor.execute(()->this.kafkaTemplate.send("in",message));
     }
 }
