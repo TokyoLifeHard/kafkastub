f9f0e5001657ca82531006efb0cb149031ca1f4b
Kawaiiskii <antonzhadnov@gmail.com>
2024-03-09 12:47:58 +0300

    refactor ReaderWriterService

--- src/main/java/com/kafkastub/kafkastub/service/ReaderWriterService.java
+++ src/main/java/com/kafkastub/kafkastub/service/ReaderWriterService.java
@@ -3,21 +3,32 @@ package com.kafkastub.kafkastub.service;
 import com.kafkastub.kafkastub.consumer.KafkaConsumer;
 import com.kafkastub.kafkastub.message.Message;
 import com.kafkastub.kafkastub.producer.KafkaProducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.Executor;
 
 @Service
 public class ReaderWriterService {
 
-    private KafkaProducer kafkaProducer;
-    private KafkaConsumer kafkaConsumer;
+    private static Logger LOGGER = LoggerFactory.getLogger(ReaderWriterService.class);
 
-    public ReaderWriterService(KafkaProducer kafkaProducer, KafkaConsumer kafkaConsumer) {
+    private KafkaProducer kafkaProducer;
+
+    @Autowired
+    @Qualifier("readPoolTaskExecutor")
+    Executor executor;
+
+    public ReaderWriterService(KafkaProducer kafkaProducer) {
         this.kafkaProducer = kafkaProducer;
-        this.kafkaConsumer = kafkaConsumer;
     }
 
     public void readWrite(Message message) {
-        kafkaProducer.send(message.toString());
-        kafkaConsumer.read(message.toString());
+        LOGGER.info(String.format("Send message %s from in to out",message));
+        executor.execute(()->kafkaProducer.send(message.toString()));
     }
 }
