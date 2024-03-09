66f0039e71ff2cbc4c866d566fa762a1e812f452
Kawaiiskii <antonzhadnov@gmail.com>
2024-03-09 12:53:52 +0300

    add logging

--- src/main/java/com/kafkastub/kafkastub/consumer/KafkaConsumer.java
+++ src/main/java/com/kafkastub/kafkastub/consumer/KafkaConsumer.java
@@ -1,12 +1,15 @@
 package com.kafkastub.kafkastub.consumer;
 
-import com.kafkastub.kafkastub.producer.KafkaProducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaConsumer {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
 
     private KafkaTemplate<String,String> kafkaTemplate;
 
@@ -19,5 +22,6 @@ public class KafkaConsumer {
     @KafkaListener(topics = {"in"})
     public void read(String data){
         kafkaTemplate.send("out",data);
+        LOGGER.info(String.format("Send message %s to in",data));
     }
 }
